1273b0f3d4f146569c8de7ca2d351c938f5ffc43
HoangVietPW26 <nhviet26@gmail.com>
2025-01-16 02:33:20 +0000

    pdf index model

--- src/bot/tasks.py
+++ src/bot/tasks.py
@@ -1,7 +1,8 @@
 from celery import shared_task
 import slack
-from . import utils
+from . import utils, ai
 from celery.utils.log import get_task_logger
+
 
 logger = get_task_logger(__name__)
 
@@ -9,14 +10,16 @@ logger = get_task_logger(__name__)
 def slack_messege_task(message, channel_id=None, user_id=None, thread_ts=None):
     logger.info("Starting task...")  # Add this to debug
     try:
-        ollama_response = utils.chat_with_ollama(message)
-        logger.info(f"Ollama response: {ollama_response}")  # Debug print
+        # response = utils.chat_with_openai(message)
+        # response = utils.chat_with_ollama(message)
+        response = ai.query(message, raw=False)
+        logger.info(f"Ollama response: {response}")  # Debug print
         
-        if ollama_response is None:  # Check if Ollama failed
+        if response is None:  # Check if Ollama failed
             logger.info("Ollama returned None")
             return None
             
-        r = slack.send_message(ollama_response, channel_id, user_id, thread_ts)
+        r = slack.send_message(response, channel_id, user_id, thread_ts)
         return r.status_code
     except Exception as e:
         logger.info(f"Error in task: {str(e)}")  # Debug print
--- src/nbs/.ipynb_checkpoints/setup-checkpoint.py
+++ src/nbs/.ipynb_checkpoints/setup-checkpoint.py
@@ -6,7 +6,7 @@ NBS_DIR = pathlib.Path(__file__).resolve().parent
 BASE_DIR = NBS_DIR.parent
 
 
-def init_django(project_name="app"):
+def init_django(project_name="cfehome"):
     os.chdir(BASE_DIR)
     sys.path.insert(0, os.getenv("PWD"))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"{project_name}.settings")
